aa783fbce37b37f69355aa1103bac3ffd97820b3
vchumachenko <v.chumachenko@yahoo.com>
2023-09-27 15:52:45 +0300

    master Done

--- src/main/java/org/nox/Main.java
+++ src/main/java/org/nox/Main.java
@@ -1,7 +1,34 @@
 package org.nox;
 
+/*Первая итерация цикла for:
+
+* Создается объект MyException внутри блока try.
+* В статическом блоке init() снова создается объект MyException, что вызывает исключение.
+* Это исключение поймано в блоке catch. Выводится сообщение "e = " с информацией о перехваченном исключении.
+* Затем проверяется, является ли причина исключения объектом MyException,
+* и выводится сообщение "e instanceof MyException, s = Hello" с вызовом метода ex.get(), который возвращает "Hello".
+
+
+* Вторая итерация цикла for:
+
+*Все шаги повторяются точно так же, как и в первой итерации, и будет выведено то же самое сообщение.*/
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+  public static void main(String[] args) {
+
+    for (int i = 0; i < 2; i++) {
+      try {
+        new MyException();
+      } catch (Throwable e) {
+        System.out.println("e = " + e);
+        // Проверка, является ли причина исключения объектом типа MyException.
+        if (e.getCause() instanceof MyException) {
+          MyException ex = (MyException) e.getCause(); // Преобразование причины в объект MyException.
+          // Вывод о том, что причина является объектом MyException и сообщение из этой причины с
+          // помощью ex.get().
+          System.out.println("e instanceof " + MyException.class.getName() + ", s = " + ex.get());
+        }
+      }
     }
+  }
 }
--- src/main/java/org/nox/MyException.java
+++ src/main/java/org/nox/MyException.java
@@ -1,2 +1,21 @@
-package org.nox;public class MyException {
+package org.nox;
+
+public class MyException extends RuntimeException {
+  private String s;
+
+  static {
+    init();
+  }
+
+  public MyException() {
+    s = "Hello";
+  }
+
+  private static void init() {
+    throw new MyException();
+  }
+
+  public String get() {
+    return s;
+  }
 }
